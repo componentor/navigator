7603c62958e4106edf4f156ce5ecf2d2ff73b747
Vue Play <author@vueplay.com>
2025-01-10 17:03:58 +0100

    Stored by Vue Play

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,10 +1,21 @@
 <template>
-	<Demo iconSize="145px"></Demo>
+	<Demo iconSize="145px">
+		<NavItem></NavItem>
+		<NavItem>
+			<NavItem></NavItem>
+			<NavItem></NavItem>
+			<NavItem></NavItem>
+		</NavItem>
+		<NavItem></NavItem>
+		<NavItem></NavItem>
+	</Demo>
 </template>
 <script>
+	import NavItem from '@/components/NavItem.vue';
 	import Demo from '@/App.vue';
 	export default {
 		components: {
+			NavItem: NavItem,
 			Demo: Demo
 		},
 		data: () => ({})
